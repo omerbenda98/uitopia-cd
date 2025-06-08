08941e3a930f289335a29e392f09b037bf0cb8b1
omerbenda98 <omerbenda98@gmail.com>
2025-06-08 22:47:59 +0300

    test jenkins

--- user_data/user_data_jenkins_agent.sh
+++ user_data/user_data_jenkins_agent.sh
@@ -38,3 +38,12 @@ sudo chown ubuntu:ubuntu /home/ubuntu/jenkins
 #install ansible
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
+# Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install
+
+# Install Terraform (latest 1.8.0 as example)
+curl -LO https://releases.hashicorp.com/terraform/1.8.0/terraform_1.8.0_linux_amd64.zip
+unzip terraform_1.8.0_linux_amd64.zip
+mv terraform /usr/local/bin/
