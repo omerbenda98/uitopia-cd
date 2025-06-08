bc74abdcb60f1eb4fd66e4307cffe5b5608ff36a
omerbenda98 <omerbenda98@gmail.com>
2025-06-08 23:26:05 +0300

    adsa

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,11 +2,11 @@ resource "local_file" "inventory" {
   filename = "../ansible/inventory.ini"
 
   content = join("\n", concat(
-    ["[app]"],
+    ["[prod]"],
     [for i in range(length(module.app.ec2_private_ips)) :
       "${module.app.ec2_names[i]} ansible_host=${module.app.ec2_private_ips[i]} ansible_user=ubuntu"
     ],
-    ["[stage]"],
+    ["[dev]"],
     [for i in range(length(module.stage.ec2_private_ips)) :
       "${module.stage.ec2_names[i]} ansible_host=${module.stage.ec2_private_ips[i]} ansible_user=ubuntu"
     ]
